5c54030a58c99989b75e04e4bb149694e4ddf0b4
chanipsophie <chanip0114@gmail.com>
2022-08-10 21:53:43 -0700

    adding instrouctor code for LUU Module 04 chaper 03

--- main.cpp
+++ main.cpp
@@ -5,67 +5,183 @@
 
 using namespace std;
 
-ENetAddress address;
-ENetHost* server = nullptr;
-ENetHost* client = nullptr;
-string ServerUserName;
-string ClientUserName;
+ENetHost* NetHost = nullptr;
+ENetPeer* Peer = nullptr;
+bool IsServer = false;
+thread* PacketThread = nullptr;
 
+enum PacketHeaderTypes
+{
+    PHT_Invalid = 0,
+    PHT_IsDead,
+    PHT_Position,
+    PHT_Count
+};
+
+struct GamePacket
+{
+    GamePacket() {}
+    PacketHeaderTypes Type = PHT_Invalid;
+};
+
+struct IsDeadPacket : public GamePacket
+{
+    IsDeadPacket()
+    {
+        Type = PHT_IsDead;
+    }
+
+    int playerId = 0;
+    bool IsDead = false;
+};
+
+struct PositionPacket : public GamePacket
+{
+    PositionPacket()
+    {
+        Type = PHT_Position;
+    }
+
+    int playerId = 0;
+    int x = 0;
+    int y = 0;
+};
+
+//can pass in a peer connection if wanting to limit
 bool CreateServer()
 {
-    /* Bind the server to the default localhost.     */
-    /* A specific host address can be specified by   */
-    /* enet_address_set_host (& address, "x.x.x.x"); */
+    ENetAddress address;
     address.host = ENET_HOST_ANY;
-    /* Bind the server to port 1234. */
     address.port = 1234;
-    server = enet_host_create(&address /* the address to bind the server host to */,
+    NetHost = enet_host_create(&address /* the address to bind the server host to */,
         32      /* allow up to 32 clients and/or outgoing connections */,
         2      /* allow up to 2 channels to be used, 0 and 1 */,
         0      /* assume any amount of incoming bandwidth */,
         0      /* assume any amount of outgoing bandwidth */);
 
-    return server != nullptr;
+    return NetHost != nullptr;
 }
 
 bool CreateClient()
 {
-    client = enet_host_create(NULL /* create a client host */,
+    NetHost = enet_host_create(NULL /* create a client host */,
         1 /* only allow 1 outgoing connection */,
         2 /* allow up 2 channels to be used, 0 and 1 */,
         0 /* assume any amount of incoming bandwidth */,
         0 /* assume any amount of outgoing bandwidth */);
 
-    return client != nullptr;
+    return NetHost != nullptr;
 }
 
-void ServerSendMessage()
+bool AttemptConnectToServer()
 {
-    while (true) {
-        string msg;
-        cin >> msg;
-        string msg_to_send = ServerUserName + ":" + msg;
+    ENetAddress address;
+    /* Connect to some.server.net:1234. */
+    enet_address_set_host(&address, "127.0.0.1");
+    address.port = 1234;
+    /* Initiate the connection, allocating the two channels 0 and 1. */
+    Peer = enet_host_connect(NetHost, &address, 2, 0);
+    return Peer != nullptr;
+}
 
-        ENetPacket* packet = enet_packet_create(msg_to_send.c_str(),
-            strlen(msg_to_send.c_str()) + 1,
-            ENET_PACKET_FLAG_RELIABLE);
-        enet_host_broadcast(server, 0, packet);
-        enet_host_flush(server);
+void HandleReceivePacket(const ENetEvent& event)
+{
+    GamePacket* RecGamePacket = (GamePacket*)(event.packet->data);
+    if (RecGamePacket)
+    {
+        cout << "Received Game Packet " << endl;
+
+        if (RecGamePacket->Type == PHT_IsDead)
+        {
+            cout << "u dead?" << endl;
+            IsDeadPacket* DeadGamePacket = (IsDeadPacket*)(event.packet->data);
+            if (DeadGamePacket)
+            {
+                string response = (DeadGamePacket->IsDead ? "yeah" : "no");
+                cout << response << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "Invalid Packet " << endl;
+    }
+
+    /* Clean up the packet now that we're done using it. */
+    enet_packet_destroy(event.packet);
+    {
+        enet_host_flush(NetHost);
     }
 }
 
-void ClientSendMessage()
+void BroadcastIsDeadPacket()
 {
-    while (true) {
-        string msg;
-        cin >> msg;
-        string msg_to_send = ClientUserName + ":" + msg;
+    IsDeadPacket* DeadPacket = new IsDeadPacket();
+    DeadPacket->IsDead = true;
+    ENetPacket* packet = enet_packet_create(DeadPacket,
+        sizeof(IsDeadPacket),
+        ENET_PACKET_FLAG_RELIABLE);
 
-        ENetPacket* packet = enet_packet_create(msg_to_send.c_str(),
-            strlen(msg_to_send.c_str()) + 1,
-            ENET_PACKET_FLAG_RELIABLE);
-        enet_host_broadcast(client, 0, packet);
-        enet_host_flush(client);
+    /* One could also broadcast the packet by         */
+    enet_host_broadcast(NetHost, 0, packet);
+    //enet_peer_send(event.peer, 0, packet);
+
+    /* One could just use enet_host_service() instead. */
+    //enet_host_service();
+    enet_host_flush(NetHost);
+    delete DeadPacket;
+}
+
+void ServerProcessPackets()
+{
+    while (1)
+    {
+        ENetEvent event;
+        while (enet_host_service(NetHost, &event, 1000) > 0)
+        {
+            switch (event.type)
+            {
+            case ENET_EVENT_TYPE_CONNECT:
+                cout << "A new client connected from "
+                    << event.peer->address.host
+                    << ":" << event.peer->address.port
+                    << endl;
+                /* Store any relevant client information here. */
+                event.peer->data = (void*)("Client information");
+                BroadcastIsDeadPacket();
+                break;
+            case ENET_EVENT_TYPE_RECEIVE:
+                HandleReceivePacket(event);
+                break;
+
+            case ENET_EVENT_TYPE_DISCONNECT:
+                cout << (char*)event.peer->data << "disconnected." << endl;
+                /* Reset the peer's client information. */
+                event.peer->data = NULL;
+                //notify remaining player that the game is done due to player leaving
+            }
+        }
+    }
+}
+
+void ClientProcessPackets()
+{
+    while (1)
+    {
+        ENetEvent event;
+        /* Wait up to 1000 milliseconds for an event. */
+        while (enet_host_service(NetHost, &event, 1000) > 0)
+        {
+            switch (event.type)
+            {
+            case  ENET_EVENT_TYPE_CONNECT:
+                cout << "Connection succeeded " << endl;
+                break;
+            case ENET_EVENT_TYPE_RECEIVE:
+                HandleReceivePacket(event);
+                break;
+            }
+        }
     }
 }
 
@@ -85,72 +201,18 @@ int main(int argc, char** argv)
     cin >> UserInput;
     if (UserInput == 1)
     {
+        //How many players?
+
         if (!CreateServer())
         {
             fprintf(stderr,
-                "An error occurred while trying to create an ENet server host.\n");
+                "An error occurred while trying to create an ENet server.\n");
             exit(EXIT_FAILURE);
         }
 
-        cout << "Please enter your name" << endl;
-        cin >> ServerUserName;
-        
-        thread ServerSendingThread(ServerSendMessage);
-
-        while (1)
-        {
-            ENetEvent event;
-            /* Wait up to 1000 milliseconds for an event. */
-            while (enet_host_service(server, &event, 1000) > 0)
-            {
-                switch (event.type)
-                {
-                case ENET_EVENT_TYPE_CONNECT:
-                    cout << "A new client connected from "
-                        << event.peer->address.host
-                        << ":" << event.peer->address.port
-                        << endl;
-                    /* Store any relevant client information here. */
-                    event.peer->data = (void*)("Client information");
-
-                    {
-                        /* Create a reliable packet of size 7 containing "packet\0" */
-                        ENetPacket* packet = enet_packet_create("hello",
-                            strlen("hello") + 1,
-                            ENET_PACKET_FLAG_RELIABLE);
-                        /* Extend the packet so and append the string "foo", so it now */
-                        /* contains "packetfoo\0"                                      */
-                        //enet_packet_resize(packet, strlen("packetfoo") + 1);
-                        //strcpy(&packet->data[strlen("packet")], "foo");
-                        /* Send the packet to the peer over channel id 0. */
-                        /* One could also broadcast the packet by         */
-                        enet_host_broadcast(server, 0, packet);
-                        //enet_peer_send(event.peer, 0, packet);
-
-                        /* One could just use enet_host_service() instead. */
-                        //enet_host_service();
-                        enet_host_flush(server);
-                    }
-                    break;
-                case ENET_EVENT_TYPE_RECEIVE:
-                    cout << (char*)event.packet->data
-                        << endl;
-                    //<< "was received from " << (char*)event.peer->data
-                    //<< " on channel " << event.channelID << endl;
-                /* Clean up the packet now that we're done using it. */
-                    enet_packet_destroy(event.packet);
-
-                    break;
-
-                case ENET_EVENT_TYPE_DISCONNECT:
-                    cout << (char*)event.peer->data << "disconnected." << endl;
-                    /* Reset the peer's client information. */
-                    event.peer->data = NULL;
-                }
-            }
-        }
-
-        ServerSendingThread.join();
+        IsServer = true;
+        cout << "waiting for players to join..." << endl;
+        PacketThread = new thread(ServerProcessPackets);
     }
     else if (UserInput == 2)
     {
@@ -161,69 +223,20 @@ int main(int argc, char** argv)
             exit(EXIT_FAILURE);
         }
 
-        cout << "Please enter your name" << endl;
-        cin >> ClientUserName;
-
-        thread ClientSendingThread(ClientSendMessage);
-
-        ENetAddress address;
         ENetEvent event;
-        ENetPeer* peer;
-        /* Connect to some.server.net:1234. */
-        enet_address_set_host(&address, "127.0.0.1");
-        address.port = 1234;
-        /* Initiate the connection, allocating the two channels 0 and 1. */
-        peer = enet_host_connect(client, &address, 2, 0);
-        if (peer == NULL)
+        if (!AttemptConnectToServer())
         {
             fprintf(stderr,
                 "No available peers for initiating an ENet connection.\n");
             exit(EXIT_FAILURE);
         }
-        /* Wait up to 5 seconds for the connection attempt to succeed. */
-        if (enet_host_service(client, &event, 5000) > 0 &&
-            event.type == ENET_EVENT_TYPE_CONNECT)
+
+        PacketThread = new thread(ClientProcessPackets);
+
+        //handle possible connection failure
         {
-            cout << "Connection to 127.0.0.1:1234 succeeded." << endl;
-        }
-        else
-        {
-            /* Either the 5 seconds are up or a disconnect event was */
-            /* received. Reset the peer in the event the 5 seconds   */
-            /* had run out without any significant event.            */
-            enet_peer_reset(peer);
-            cout << "Connection to 127.0.0.1:1234 failed." << endl;
-        }
-
-        while (1)
-        {
-            ENetEvent event;
-            /* Wait up to 1000 milliseconds for an event. */
-            while (enet_host_service(client, &event, 1000) > 0)
-            {
-                switch (event.type)
-                {
-                case ENET_EVENT_TYPE_RECEIVE:
-                    cout << (char*)event.packet->data
-                        << endl;
-                    /* Clean up the packet now that we're done using it. */
-                    enet_packet_destroy(event.packet);
-
-                    {
-                        /* Create a reliable packet of size 7 containing "packet\0" */
-                        ENetPacket* packet = enet_packet_create("hi",
-                            strlen("hi") + 1,
-                            ENET_PACKET_FLAG_RELIABLE);
-
-                        enet_host_broadcast(client, 0, packet);
-                        //enet_peer_send(event.peer, 0, packet);
-
-                        /* One could just use enet_host_service() instead. */
-                        //enet_host_service();
-                        enet_host_flush(client);
-                    }
-                }
-            }
+            //enet_peer_reset(Peer);
+            //cout << "Connection to 127.0.0.1:1234 failed." << endl;
         }
     }
     else
@@ -231,16 +244,16 @@ int main(int argc, char** argv)
         cout << "Invalid Input" << endl;
     }
 
-    if (server != nullptr)
-    {
-        enet_host_destroy(server);
-    }
 
-    if (client != nullptr)
+    if (PacketThread)
     {
-        enet_host_destroy(client);
+        PacketThread->join();
     }
-
+    delete PacketThread;
+    if (NetHost != nullptr)
+    {
+        enet_host_destroy(NetHost);
+    }
 
     return EXIT_SUCCESS;
 }
